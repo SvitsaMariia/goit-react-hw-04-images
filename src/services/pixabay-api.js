0ab755af0e084321c84ed9a601e2bdd48fbe13b5
MariiaSvitsa <mariia0198@gmail.com>
2023-11-15 13:36:09 +0200

    +

--- src/services/pixabay-api.js
+++ src/services/pixabay-api.js
@@ -1,23 +1,3 @@
-// import axios from 'axios';
-
-// const API_KEY = '19502637-d06b43ee367f131773b25cf8c';
-
-// const pixabayAPI = axios.create({
-//   baseURL: 'https://pixabay.com/api/?key=19502637-d06b43ee367f131773b25cf8c&q=yellow+flowers&image_type=photo',
-// });
-
-// export const fetchImages = async ({ query, page, perPage }) => {
-//   return pixabayAPI.get('/api/', {
-//     params: {
-//       key: API_KEY,
-//       q: query,
-//       page,
-//       image_type: 'photo',
-//       orientation: 'horizontal',
-//       per_page: perPage,
-//     },
-//   });
-// };
 
 import axios from 'axios';
 
@@ -26,7 +6,7 @@ const apiCreate = axios.create({
 });
 
 export const imagesApi = async searchParams => {
-  // console.log(searchParams);
+
   return await apiCreate.get('', {
     params: {
       key: '19502637-d06b43ee367f131773b25cf8c',
