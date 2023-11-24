c1c3ea37c0b402cfa31929690cd5ee5e8ce5037a
MariiaSvitsa <mariia0198@gmail.com>
2023-11-24 10:48:30 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,136 @@
+// import { SearchBar } from './SearchBar/SearchBar';
+// import { imagesApi } from 'services/pixabay-api';
+// import { ImageGallery } from './ImageGallery/ImageGallery';
+// import { Button } from './Button/Button';
+// import { Loader } from './Loader/Loader';
+// import { useState, useEffect } from 'react';
+
+// export const App = () => {
+//   const [q, setQ] = useState('');
+//   const [page, setPage] = useState(1);
+//   const [loading, setLoading] = useState(false);
+//   const [images, setImages] = useState([]);
+//   const [btn, setBtn] = useState(false);
+
+//   const handleChangeQuery = newQ => {
+//     setImages([]);
+//     setPage(1);
+//     setQ(newQ);
+//   };
+//   useEffect(() => {
+//     if (!q) {
+//       return;
+//     }
+//     const getData = async () => {
+//       try {
+//         setLoading(true);
+//         const data = await imagesApi({ q, page });
+//         setImages(prev => [...prev, ...data.data.hits]);
+
+//         setBtn(page < Math.ceil(data.data.totalHits / 12));
+//       } catch (err) {
+//         console.log(err);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     getData();
+//   }, [q, page]);
+
+//   const handleBtnClick = () => {
+//     setPage(prev => prev + 1);
+//   };
+
+//   return (
+//     <>
+//       <SearchBar onSubmit={handleChangeQuery} />
+
+//       {loading && <Loader />}
+
+//       <ImageGallery arr={images} />
+
+//       {btn && <Button cb={handleBtnClick} />}
+//     </>
+//   );
+// };
+
+
 import { SearchBar } from './SearchBar/SearchBar';
 import { imagesApi } from 'services/pixabay-api';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 import { Loader } from './Loader/Loader';
 import { useState, useEffect } from 'react';
+import { Modal } from './Modal/Modal';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import 'index.css';
 
-export const App = () => {
-  const [q, setQ] = useState('');
-  const [page, setPage] = useState(1);
+
+export function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [gallery, setGallery] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [images, setImages] = useState([]);
-  const [btn, setBtn] = useState(false);
+  const [page, setPage] = useState(1);
+  const [showModal, setShowModal] = useState(false);
+  const [modalImg, setModalImg] = useState('');
 
-  const handleChangeQuery = newQ => {
-    setImages([]);
-    setPage(1);
-    setQ(newQ);
-  };
   useEffect(() => {
-    if (!q) {
+    if (!inputValue) {
       return;
     }
-    const getData = async () => {
-      try {
-        setLoading(true);
-        const data = await imagesApi({ q, page });
-        setImages(prev => [...prev, ...data.data.hits]);
 
-        setBtn(page < Math.ceil(data.data.totalHits / 12));
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setLoading(false);
-      }
-    };
+    setLoading(true);
 
-    getData();
-  }, [q, page]);
+    imagesApi(inputValue, page)
+      .then(response => {
+        setGallery(state => [...state, ...response.data.hits]);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(() => setLoading(false));
+  }, [inputValue, page]);
 
-  const handleBtnClick = () => {
-    setPage(prev => prev + 1);
+  const nextPage = () => {
+    setPage(state => state + 1);
+    scroll.scrollToBottom();
+  };
+
+  const handleFormSubmit = value => {
+    setInputValue(value);
+    setGallery([]);
+    setPage(1);
+  };
+
+  const LargeImg = large => {
+    setModalImg(large);
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
   return (
-    <>
-      <SearchBar onSubmit={handleChangeQuery} />
-
+    <div className="App">
+      <SearchBar onSubmit={handleFormSubmit} />
+      {gallery.length > 0 && (
+        <ImageGallery
+          options={gallery}
+          onClick={toggleModal}
+          modalImg={LargeImg}
+        />
+      )}
       {loading && <Loader />}
+      {gallery.length > 0 && !loading && <Button nextPage={nextPage} />}
+      {showModal && (
+        <Modal onClick={toggleModal}>
+          <img style={{ width: 1000 }} src={modalImg} alt="modal" />
+        </Modal>
+      )}
 
-      <ImageGallery arr={images} />
-
-      {btn && <Button cb={handleBtnClick} />}
-    </>
+      <ToastContainer autoClose={3000} />
+    </div>
   );
-};
+}
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,39 +1,17 @@
-import css from './SearchBar.module.css';
-import { useState } from 'react';
-import propTypes from 'prop-types';
+import React from 'react';
+import SearchForm from './SearchForm/SearchForm';
+import PropTypes from 'prop-types';
 
-export const SearchBar = ({ onSubmit }) => {
-  const [inputValue, setInputValue] = useState('');
-  const handleChangeValue = ({ target }) => {
-    setInputValue(target.value);
-  };
-  const handleSubmit = e => {
-    e.preventDefault();
-    onSubmit(inputValue);
-    setInputValue('');
-  };
+import styles from './SearchBar.module.css';
+
+export default function SearchBar({ onSubmit }) {
   return (
-    <header className={css.SearchBar}>
-      <form className={css.SearchForm} onSubmit={handleSubmit}>
-        <button type="css" className={css.SearchForm_button}>
-          <span className={css.button_label}>Search</span>
-        </button>
-
-        <input
-          className={CSS.SearchForm_input}
-          type="text"
-          autoComplete="off"
-          value={inputValue}
-          onChange={handleChangeValue}
-          autoFocus
-          placeholder="Search images and photos"
-          name="searcher"
-        />
-      </form>
+    <header className={styles.SearchBar}>
+      <SearchForm onSubmit={onSubmit} />
     </header>
   );
-};
+}
 
 SearchBar.propTypes = {
-  onSubmit: propTypes.func,
+  onSubmit: PropTypes.func.isRequired,
 };
